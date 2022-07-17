f821f47f4dce4a6d0a10a891fd2a0452b28e5113
Josh Graff <Github2@joshuagraff.com>
2022-07-17 12:59:02 -0700

    update import of 2.7 display

--- epaper-clock-and-more/epds/epd2in7b.py
+++ epaper-clock-and-more/epds/epd2in7b.py
@@ -24,14 +24,14 @@
  # THE SOFTWARE.
  #
 
-import epdif
+import epds.epdif
 from PIL import Image
 from PIL import ImageDraw
 import RPi.GPIO as GPIO
 
 # Display resolution
-EPD_WIDTH       = 176
-EPD_HEIGHT      = 264
+EPD_WIDTH       = 400
+EPD_HEIGHT      = 300
 
 # EPD2IN7B commands
 PANEL_SETTING                               = 0x00
