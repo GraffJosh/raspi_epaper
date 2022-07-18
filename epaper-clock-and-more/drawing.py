676554de44f939dbe825e1a43de17cc2e519992d
Josh Graff <Github2@joshuagraff.com>
2022-07-17 17:58:48 -0700

    attempting caltrain

--- epaper-clock-and-more/drawing.py
+++ epaper-clock-and-more/drawing.py
@@ -220,6 +220,7 @@ class Drawing(object):
 
     def draw_train_eta(self, idx, black_buf, red_buf, caltrain, warn_above_percent, black_on_red):
         start_pos = (50  + ((idx + 1) * self.CANVAS_WIDTH) / 3, 100)
+        print(caltrain.departure_time)
         time_to_next_departure = (time.mktime(time.localtime()) - time.mktime(caltrain.departure_time)) / 60
         if time_to_next_departure < warn_above_percent:
             no_warn = False
@@ -235,7 +236,7 @@ class Drawing(object):
 
         draw = ImageDraw.Draw(buf)
 
-        caption = "%2i" % int(round(time_to_next_departure / 60))
+        caption = "%2i" % int(round(time_to_next_departure))
 
         if not no_warn and black_on_red:
             black_draw = ImageDraw.Draw(black_buf)
