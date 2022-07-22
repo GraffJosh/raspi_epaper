988b1887c0527dcadfee8aa0fae5eb53326f4407
Josh Graff <Github2@joshuagraff.com>
2022-07-21 22:51:00 -0700

    going to attempt quick refresh

--- epaper-clock-and-more/epds/epd4in2b_V2.py
+++ epaper-clock-and-more/epds/epd4in2b_V2.py
@@ -292,7 +292,7 @@ class EPD:
         for i in range(0, int(self.width * self.height / 8)):
             self.send_data(imagered[i])
         
-        self.set_lut(quick=(self.fast_count < self.max_fast_refresh or self.fast_count == 0))
+        # self.set_lut(quick=(self.fast_count < self.max_fast_refresh or self.fast_count == 0))
 
         self.send_command(DISPLAY_REFRESH) 
         epdconfig.delay_ms(20)
